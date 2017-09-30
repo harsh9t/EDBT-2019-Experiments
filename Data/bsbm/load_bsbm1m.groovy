1a4b6523766790004648f363cfbc700680c53efc
harsh9t <harsh9t@gmail.com>
2017-09-30 11:56:32 +0200

    adding structure

---
+++ Data/bsbm/load_bsbm1m.groovy
@@ -0,0 +1,13 @@
+class BSBM1M {
+
+  public static Graph createGraph() {
+	def newGraph = TinkerGraph.open()
+	def file = new File('./bin/bsbm1m.graphml')	
+	BSBM1M.load(newGraph, file.getAbsolutePath())
+	return newGraph
+  }
+
+  public static void load(final Graph newGraph, final String path) {
+    newGraph.io(IoCore.graphml()).readGraph(path)
+  }
+}
