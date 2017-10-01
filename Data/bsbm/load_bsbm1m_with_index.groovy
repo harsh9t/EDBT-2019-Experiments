de4fe0b4a2c8a5d5ff294da47b80fd917cf5eb4b
harsh9t <harsh9t@gmail.com>
2017-10-01 15:18:23 +0200

    adding groovy files with and without index versions

---
+++ Data/bsbm/load_bsbm1m_with_index.groovy
@@ -0,0 +1,15 @@
+class BSBM1M {
+
+  public static Graph createGraph() {
+	def newGraph = TinkerGraph.open()
+	def file = new File('./bin/bsbm1m.graphml')
+	BSBM1M.load(newGraph, file.getAbsolutePath())
+	return newGraph
+  }
+
+  public static void load(final Graph newGraph, final String path) {
+    newGraph.io(IoCore.graphml()).readGraph(path)
+    newGraph.createIndex('productID', Vertex.class)
+    newGraph.createIndex('label_n', Vertex.class)
+  }
+}
