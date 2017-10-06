f61782bc3c91646d8909ee9f30f42d736d6869c7
Yashwant Keswani <yashwant.keswani@gmail.com>
2017-10-06 19:35:01 +0200

    Configurable generator

--- generator.py
+++ generator.py
@@ -43,6 +43,30 @@ System.exit(0);
 """ %(vmap[graph], str(random.randint(0,1000000000)), dmap[dataset], indexing_syn, query))
 	f.close()
 
+def create_warm_query(query, graph, dataset, path, index = None):
+	indexing_syn = ""
+	if index is not None:
+		if graph == "sparksee":
+			for each in index:
+				indexing_syn += 's.createKeyIndex("%s", Vertex.class)\n' %(each);
+		else:
+			for each in index:
+				indexing_syn += 's.createIndex("%s", Vertex.class)\n' %(each);
+
+	f = open(path + ".groovy" , "w")
+	f.write(""" import com.tinkerpop.blueprints.impls.sparksee.*;
+s = new %s("/tmp/%s");
+s.loadGraphML("%s");
+%s
+for(i in 1..10) {
+	q = System.currentTimeMillis();
+	tt = %s
+	println (System.currentTimeMillis() - q)
+}
+System.exit(0);
+""" %(vmap[graph], str(random.randint(0,1000000000)), dmap[dataset], indexing_syn, query))
+	f.close()
+
 def create_query_for_tinker3(query, dataset, path, index = None):
 	indexing_syn = ""
 	if index is not None:
@@ -61,7 +85,27 @@ println (System.currentTimeMillis() - q)
 """ %("/srv/Harsh/Yashwant/edbt18/EDBT-2018-Experiments/data/northwind.kryo", indexing_syn, query))
 	f.close()
 
-def create_queries(dataset_location, index = None):
+def create_warm_query_for_tinker3(query, dataset, path, index = None):
+	indexing_syn = ""
+	if index is not None:
+		for each in index:
+			indexing_syn += 'x.createIndex("%s", Vertex.class)\n' %(each);
+
+	f = open(path + ".groovy" , "w")
+	f.write("""
+x = TinkerGraph.open();
+x.io(gryo()).readGraph("%s");
+%s
+g = x.traversal();
+for (i in 1..10) {
+	q = System.currentTimeMillis();
+	tt = %s
+	println (System.currentTimeMillis() - q)
+}
+""" %("/srv/Harsh/Yashwant/edbt18/EDBT-2018-Experiments/data/northwind.kryo", indexing_syn, query))
+	f.close()
+
+def create_queries(dataset_location, index = None, warm = True):
 	all_queries = glob.glob(dataset_location+"/gremlin2_raw/*")
 	for each in ['neo4j', 'sparksee', 'tinkergraph']:
 		try:
@@ -69,19 +113,25 @@ def create_queries(dataset_location, index = None):
 		except Exception as e:
 			clear_folder(dataset_location + '/%s' % (each))
 		for query in all_queries:
-			create_query(open(query).read(), each, dataset_location.strip().split("/")[-1], dataset_location + '/%s/%s' % (each, get_file_name(query)), index)
-	
-def create_queries_3(dataset_location, index = None):
+			if warm:
+				create_warm_query(open(query).read(), each, dataset_location.strip().split("/")[-1], dataset_location + '/%s/%s' % (each, get_file_name(query)), index)
+			else:
+				create_query(open(query).read(), each, dataset_location.strip().split("/")[-1], dataset_location + '/%s/%s' % (each, get_file_name(query)), index)
+
+def create_queries_3(dataset_location, index = None, warm = True):
 	try:
 		os.mkdir(dataset_location + '/tinker3')
 	except Exception as e:
 		clear_folder(dataset_location + '/tinker3')
 	tinker3_queries = glob.glob(dataset_location+"/gremlin3_raw/*")
 	for query in tinker3_queries:
-		create_query_for_tinker3(open(query).read(), dataset_location.strip().split("/")[-1], dataset_location + '/tinker3/%s' % (get_file_name(query)), index);
+		if warm:
+			create_warm_query_for_tinker3(open(query).read(), dataset_location.strip().split("/")[-1], dataset_location + '/tinker3/%s' % (get_file_name(query)), index);
+		else:
+			create_query_for_tinker3(open(query).read(), dataset_location.strip().split("/")[-1], dataset_location + '/tinker3/%s' % (get_file_name(query)), index);
 
 if __name__ == "__main__":
-	create_queries(os.getcwd() + "/nwq", ['name', 'customerId', 'unitPrice', 'unitsInStock', 'unitsOnOrder'])
-	create_queries(os.getcwd() + "/bsbmq", ["productID", "label_n", "type", "productTypeID", "reviewerID"])
-	create_queries_3(os.getcwd() + "/nwq", ['name', 'customerId', 'unitPrice', 'unitsInStock', 'unitsOnOrder'])
-	create_queries_3(os.getcwd() + "/bsbmq", ["productID", "label_n", "type", "productTypeID", "reviewerID"])	
+	create_queries(os.getcwd() + "/nwq", index = ['name', 'customerId', 'unitPrice', 'unitsInStock', 'unitsOnOrder'], warm = True)
+	create_queries(os.getcwd() + "/bsbmq", index = ["productID", "label_n", "type", "productTypeID", "reviewerID"], warm = True)
+	create_queries_3(os.getcwd() + "/nwq", index = ['name', 'customerId', 'unitPrice', 'unitsInStock', 'unitsOnOrder'], warm = True)
+	create_queries_3(os.getcwd() + "/bsbmq", index = ["productID", "label_n", "type", "productTypeID", "reviewerID"], warm = True)	
